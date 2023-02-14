88416b2a23c7df4d194a0895337646d197cca5cc
Timmy-Edibo <timothyedibo@gmail.com>
2023-02-14 12:26:39 +0100

    transaction in golang

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -12,16 +12,30 @@ import (
 const (
 	dbDriver = "postgres"
 	dbSource = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	// dbSource = "postgres://postgres:postgres@172.17.131.145:5432/simple_bank?sslmode=disable"
+
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("Cannot cannect to the database: ", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 	os.Exit(m.Run())
 }
+
+// func TestMain(m *testing.M) {
+// 	conn, err := sql.Open(dbDriver, dbSource)
+// 	if err != nil {
+// 		log.Fatal("Cannot cannect to the database: ", err)
+// 	}
+
+// 	testQueries = New(conn)
+// 	os.Exit(m.Run())
+// }
